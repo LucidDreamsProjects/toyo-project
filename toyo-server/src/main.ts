3da79cecf3356b14536dfacd83820711c3077811
Cyrnismo <cyrne18@gmail.com>
2021-07-26 04:04:19 -0300

    feat: enabled cors

--- toyo-server/src/main.ts
+++ toyo-server/src/main.ts
@@ -4,19 +4,13 @@ import {
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
-/* import { FastifyCookieOptions } from 'fastify-cookie';
-import cookie from 'fastify-cookie'; */
-import fastify from 'fastify';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
   );
-  /* app.register(cookie, {
-    secret: '',
-    parseOptions: {},
-  } as FastifyCookieOptions); */
+  app.enableCors();
   await app.listen(`${process.env.PORT}`, 'localhost');
 }
 bootstrap();
