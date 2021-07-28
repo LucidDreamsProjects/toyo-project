80d3cfd5a8d39515ed3ba2d4735d0a9c69e2562d
Cyrnismo <cyrne18@gmail.com>
2021-07-28 04:27:15 -0300

    dev: applied redirection after google auth

--- toyo-server/src/auth/controllers/auth.controller.ts
+++ toyo-server/src/auth/controllers/auth.controller.ts
@@ -2,6 +2,7 @@ import { Request } from 'express';
 import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from '../services/auth.service';
 import { Controller, Get, UseGuards, Req } from '@nestjs/common';
+import { Redirect } from '@nestjs/common';
 
 @Controller('auth')
 export class AuthController {
@@ -13,6 +14,7 @@ export class AuthController {
 
   @Get('google/callback')
   @UseGuards(AuthGuard('google'))
+  @Redirect('http://localhost:3000')
   googleAuthRedirect(@Req() req: Request) {
     return this.authService.googleLogin(req);
   }
