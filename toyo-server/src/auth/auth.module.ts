131e2fe51c2c81ed112b0f52b2866ffd8d99a20a
Cyrnismo <cyrne18@gmail.com>
2021-08-18 03:51:58 -0300

    feat: generating bearer token on startup

--- toyo-server/src/auth/auth.module.ts
+++ toyo-server/src/auth/auth.module.ts
@@ -1,7 +1,9 @@
-import { Module } from '@nestjs/common';
+import { Module, OnModuleInit } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
-import { AuthController } from './controllers/auth.controller';
 import { AuthService } from './services/auth.service';
+import { RedisCacheModule } from '../cache/redisCache.module';
+import { AuthController } from './controllers/auth.controller';
+import { RedisCacheService } from 'src/cache/redisCache.service';
 
 @Module({
   imports: [
@@ -9,9 +11,32 @@ import { AuthService } from './services/auth.service';
       timeout: 7500,
       maxRedirects: 5,
     }),
+    RedisCacheModule,
   ],
   controllers: [AuthController],
   providers: [AuthService],
   exports: [AuthService],
 })
-export class AuthModule {}
+export class AuthModule implements OnModuleInit {
+  constructor(
+    private readonly redisCacheService: RedisCacheService,
+    private readonly authService: AuthService,
+  ) {}
+
+  private async getBearerToken() {
+    return await this.authService.getBearerToken();
+  }
+
+  async onModuleInit() {
+    const credentials = await this.getBearerToken();
+    // console.log(credentials);
+
+    const access_token = credentials.access_token;
+    const refresh_token = credentials.refresh_token;
+    const session_state = credentials.session_state;
+
+    await this.redisCacheService.set('access_token', access_token);
+    await this.redisCacheService.set('refresh_token', refresh_token);
+    await this.redisCacheService.set('session_state', session_state);
+  }
+}
