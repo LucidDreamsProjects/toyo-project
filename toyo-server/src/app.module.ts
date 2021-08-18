a92154662ded649334e81b664cde07e81e114df0
Cyrnismo <cyrne18@gmail.com>
2021-08-18 03:51:07 -0300

    ref: minor changes, trials and cleanup

--- toyo-server/src/app.module.ts
+++ toyo-server/src/app.module.ts
@@ -1,8 +1,5 @@
-import { Module, CacheModule, OnModuleInit } from '@nestjs/common';
-import redisStore from 'cache-manager-redis-store';
+import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppService } from './app.service';
-import { AppController } from './app.controller';
 import { PlayerModule } from './player/player.module';
 import { Player } from './player/entities/player.entity';
 import { AuthModule } from './auth/auth.module';
@@ -10,21 +7,12 @@ import { config } from 'dotenv';
 import { ContractModule } from './contract/contract.module';
 import { NftModule } from './nft/nft.module';
 import { WalletModule } from './wallet/wallet.module';
-import { AuthService } from './auth/services/auth.service';
-import { WalletService } from './wallet/services/wallet.service';
-import { PlayerService } from './player/services/player.service';
-import { ContractService } from './contract/services/contract.service';
-import { NftService } from './nft/services/nft.service';
+import { RedisCacheModule } from './cache/redisCache.module';
 
 config();
 
 @Module({
   imports: [
-    AuthModule,
-    ContractModule,
-    NftModule,
-    WalletModule,
-    PlayerModule,
     TypeOrmModule.forRoot({
       type: 'mysql' as any,
       host: '162.240.6.22',
@@ -37,25 +25,12 @@ config();
       maxQueryExecutionTime: 1000,
       synchronize: false,
     }),
-    CacheModule.register({
-      store: redisStore,
-      host: 'localhost',
-      port: 5003,
-    }),
+    RedisCacheModule,
+    AuthModule,
+    WalletModule,
+    PlayerModule,
+    ContractModule,
+    NftModule,
   ],
-  providers: [AppService],
-  controllers: [AppController],
 })
-export class AppModule implements OnModuleInit {
-  constructor(
-    private readonly authService: AuthService,
-    private readonly walletService: WalletService,
-    private readonly playerService: PlayerService,
-    private readonly contractService: ContractService,
-    private readonly nftService: NftService,
-  ) {}
-
-  onModuleInit() {
-    console.log('The module has been initialized.');
-  }
-}
+export class AppModule {}
