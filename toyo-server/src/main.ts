67b5e880fb0cc82012a5ad0da5335aa499df725a
Cyrnismo <cyrne18@gmail.com>
2021-08-28 00:49:31 -0300

    ref: minor semantic changes...

--- toyo-server/src/main.ts
+++ toyo-server/src/main.ts
@@ -2,6 +2,7 @@ import { config } from 'dotenv';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { ValidationPipe } from '@nestjs/common';
 
 config();
 
@@ -12,6 +13,17 @@ async function bootstrap() {
   app.enableCors();
 
   app.init();
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
 
   await app.listen(`${process.env.PORT}`, () => {
     console.log(`⚡️ [server]: Server is running at http://localhost:${PORT}`);
