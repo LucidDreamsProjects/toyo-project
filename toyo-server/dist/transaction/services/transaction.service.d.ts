9fcd18bec6c89ed1c9d7cfcb52e612f9fde92014
Cyrnismo <cyrne18@gmail.com>
2021-09-19 19:32:09 -0300

    dev: updating state

--- toyo-server/dist/transaction/services/transaction.service.d.ts
+++
@@ -1,8 +0,0 @@
-import { AuthService } from '../../auth/services/auth.service';
-export declare class TransactionService {
-    private authService;
-    private readonly BASE_URL;
-    constructor(authService: AuthService);
-    getFees(secretType: string): Promise<Record<string, string | number>>;
-    getTxStatus(secretType: string, transactionHash: string): Promise<Record<string, string | number>>;
-}
