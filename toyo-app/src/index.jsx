dd1f5c188ca60051ef8f1c2fe176da3b627c9d04
Cyrnismo <cyrne18@gmail.com>
2021-09-20 16:59:24 -0300

    dev: updating state

--- toyo-app/src/index.jsx
+++ toyo-app/src/index.jsx
@@ -5,7 +5,7 @@ import { ArkaneConnect } from "@arkane-network/arkane-connect";
 import { Home } from "./views/home/home";
 import { FunctionReferences } from "./views/functionReferences/functionReferences";
 import { NoMatch } from "./views/noMatch/noMatch";
-import { AdminPanel } from "./views/adminPanel/adminPanel";
+import { SecretPanel } from "./views/secretPanel/secretPanel";
 import { AlternativePanel } from "./views/alternativePanel/alternativePanel";
 
 import "./index.css";
