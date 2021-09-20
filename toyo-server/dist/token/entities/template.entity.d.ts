dfda7833c0af7d7a73a3904672ab9791e9d0721f
Cyrnismo <cyrne18@gmail.com>
2021-09-19 21:00:12 -0300

    dev: updating state

--- toyo-server/dist/token/entities/template.entity.d.ts
+++
@@ -1,6 +0,0 @@
-import { BaseEntity } from 'typeorm';
-export declare class Template extends BaseEntity {
-    templateId?: number;
-    contractId?: number;
-    name?: string;
-}
--- toyo-server/dist/wallet/controllers/wallet.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { Wallet } from '@arkane-network/arkane-connect';
-import { WalletService } from '../services/wallet.service';
-export declare class WalletController {
-    private readonly walletService;
-    constructor(walletService: WalletService);
-    getWalletById(walletId: string): Promise<Wallet | void>;
-}
