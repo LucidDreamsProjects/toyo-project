1c26ad04786f7e33b8c1e869f3e25ccea2de56fc
Cyrnismo <cyrne18@gmail.com>
2021-09-21 12:40:58 -0300

    dev: updating state

--- toyo-app/src/index.jsx
+++ toyo-app/src/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import { ArkaneConnect } from "@arkane-network/arkane-connect";
 import { Home } from "./views/home/home";
 import { FunctionReferences } from "./views/functionReferences/functionReferences";
@@ -21,7 +21,14 @@ const App = () => {
 
   return (
     <Switch>
-      <Route exact path="/">
+      <Route
+        path="/"
+        component={() => {
+          window.location.href = "https://toyofight.io";
+          return null;
+        }}
+      />
+      <Route path="/ui-staging">
         <Home />
       </Route>
       <Route path="/alternative-panel">
