e7a133229e371d3a429ffd53278d676ae926d9c7
Cyrnismo <cyrne18@gmail.com>
2021-09-30 04:50:37 -0300

    feat: ui v2.0 being implemented (wip)

--- toyo-app/src/index.jsx
+++ toyo-app/src/index.jsx
@@ -29,15 +29,15 @@ const App = () => {
   return (
     <>
       <Switch>
-        {/* <Route
+        <Route
           exact
           path="/"
           component={() => {
             window.location.href = "https://toyofight.io";
             return null;
           }}
-        /> */}
-        <Route exact path="/">
+        />
+        <Route path="/ui-staging">
           <Home />
         </Route>
         <Route path="/webgl-staging">
