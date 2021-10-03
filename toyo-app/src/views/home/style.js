885cc28c1d7d2c6b21d7175b507415c43546893a
Cyrnismo <cyrne18@gmail.com>
2021-10-03 16:53:40 -0300

    dev: UI improvements and new box

--- toyo-app/src/views/home/style.js
+++ toyo-app/src/views/home/style.js
@@ -45,6 +45,8 @@ const Body = styled.div`
     }
 
     @media screen and (min-width: 1280px) {
+      display: none;
+
       img {
         width: 5rem;
         height: 5rem;
@@ -126,7 +128,7 @@ const Body = styled.div`
 
         img {
           position: relative;
-          left: -12.5rem;
+          left: -13rem;
           width: 800%;
           height: auto;
         }
@@ -170,7 +172,150 @@ const Body = styled.div`
       }
     }
 
-    @media screen and (min-width: 768px) and (min-height: 1024px) {
+    @media screen and (max-height: 667px) {
+      #top--col {
+        left: -2rem;
+        #top--logo {
+          img {
+            left: -12rem;
+          }
+        }
+
+        #top--btn {
+          img {
+            top: 8rem;
+            left: -10rem;
+          }
+        }
+      }
+    }
+
+    @media screen and (max-width: 375px) and (min-height: 668px) {
+      #top--bg {
+        height: 100%;
+        /* clip-path: polygon(0 0, 100% 0, 100% 50%, 0 100%); */
+        /* background: yellow; */
+        z-index: 10;
+
+        img {
+          width: 500%;
+          height: 100vh;
+        }
+      }
+
+      #top--lucido {
+        img {
+          top: 14rem;
+        }
+      }
+
+      #top--col {
+        #top--btn {
+          img {
+            top: 4rem;
+            left: -10rem;
+            width: 600%;
+          }
+        }
+      }
+
+      #top--ninja {
+        img {
+          top: 8rem;
+        }
+      }
+    }
+
+    @media screen and (min-width: 376px) and (max-width: 390px) {
+      #top--col {
+        left: -1rem;
+      }
+    }
+
+    @media screen and (min-height: 667px) and (max-height: 760px) {
+      #top--col {
+        #top--logo {
+          top: 1rem;
+          left: 1rem;
+        }
+
+        #top--btn {
+          img {
+            left: -9.5rem;
+          }
+        }
+      }
+    }
+
+    @media screen and (min-height: 761px) and (max-height: 800px) {
+      #top--lucido {
+        top: 2rem;
+      }
+
+      #top--col {
+        #top--logo {
+          top: 1rem;
+          img {
+            left: -12rem;
+          }
+        }
+      }
+
+      #top--ninja {
+        top: 2rem;
+      }
+    }
+
+    @media screen and (min-height: 800px) and (max-height: 854px) {
+      #top--bg {
+        img {
+          height: 105vh;
+        }
+      }
+
+      #top--lucido {
+        img {
+          top: 16rem;
+          left: -15rem;
+        }
+      }
+
+      #top--col {
+        #top--logo {
+          top: 0rem;
+        }
+
+        #top--btn {
+          top: -12rem;
+        }
+      }
+
+      #top--ninja {
+        img {
+          top: 8rem;
+          left: -4.5rem;
+        }
+      }
+    }
+
+    @media screen and (min-width: 412px) and (max-height: 940px) {
+      #top--lucido {
+        img {
+          top: 18rem;
+          left: -15rem;
+        }
+      }
+
+      #top--col {
+        left: -3rem;
+      }
+
+      #top--ninja {
+        top: 5rem;
+      }
+    }
+
+    @media screen and (min-width: 768px) and (max-height: 1080px) {
       #top--bg {
         width: 100%;
         height: 100%;
@@ -183,7 +328,7 @@ const Body = styled.div`
 
       #top--lucido {
         img {
-          top: 18rem;
+          top: 21rem;
           left: -26rem;
         }
       }
@@ -191,14 +336,16 @@ const Body = styled.div`
       #top--col {
         #top--logo {
           img {
-            left: -21rem;
+            top: 2rem;
+            left: -23rem;
             width: 400%;
           }
         }
 
         #top--btn {
           img {
-            left: -16rem;
+            top: 8rem;
+            left: -18rem;
             width: 300%;
           }
         }
@@ -207,6 +354,48 @@ const Body = styled.div`
       #top--ninja {
         top: 6rem;
         left: -4rem;
+      }
+    }
+
+    @media screen and (min-width: 1024px) and (min-height: 768px) {
+      #top--bg {
+        img {
+          width: 100%;
+          height: 125vh;
+        }
+      }
+
+      #top--lucido {
+        img {
+          top: 10rem;
+          left: -20rem;
+          width: 700%;
+        }
+      }
+
+      #top--col {
+        #top--logo {
+          top: -3rem;
+
+          img {
+            left: -12rem;
+            width: 200%;
+          }
+        }
+
+        #top--btn {
+          img {
+            width: 150%;
+            left: -9rem;
+          }
+        }
+      }
+
+      #top--ninja {
+        img {
+          top: 2rem;
+          width: 700%;
+        }
       }
     }
 
@@ -357,7 +546,7 @@ const Body = styled.div`
     flex-direction: column;
     width: 100%;
     min-height: 205vh;
-    max-height: 290vh;
+    max-height: 215vh;
     /* background: yellow; */
 
     #drop--bg {
@@ -467,8 +656,210 @@ const Body = styled.div`
       }
     }
 
-    @media screen and (min-width: 768px) and (min-height: 1024px) {
-      height: 240vh;
+    @media screen and (max-height: 667px) {
+      min-height: 225vh;
+      max-height: 235vh;
+
+      #drop--bg {
+        img {
+          height: 88%;
+        }
+      }
+
+      #drop--paragraph {
+        top: 84rem;
+      }
+
+      #drop--image {
+        #drop--toyo--title {
+          top: 33.5rem;
+        }
+
+        #drop--counter {
+          top: 58rem;
+        }
+      }
+
+      #drop--samurai--bg {
+        img {
+          top: 14rem;
+        }
+      }
+    }
+
+    @media screen and (min-width: 376px) and (max-height: 667px) {
+      #drop--image {
+        #drop--toyo--title {
+          top: 34rem;
+          height: 8rem;
+        }
+      }
+    }
+
+    @media screen and (min-height: 668px) and (max-height: 760px) {
+      height: 190vh;
+      max-height: 200vh;
+
+      #drop--bg {
+        img {
+          height: 84.5%;
+        }
+      }
+
+      #drop--paragraph {
+        top: 86rem;
+      }
+
+      #drop--image {
+        top: -5rem;
+
+        #drop--toyo--title {
+          top: 32rem;
+          height: 8rem;
+        }
+
+        #drop--counter {
+          top: 61rem;
+        }
+      }
+
+      #drop--samurai--bg {
+        img {
+          top: 16rem;
+        }
+      }
+    }
+
+    @media screen and (min-height: 761px) and (max-height: 800px) {
+      min-height: 170vh;
+      max-height: 180vh;
+
+      #drop--bg {
+        img {
+          height: 85.5%;
+        }
+      }
+
+      #drop--paragraph {
+        top: 86rem;
+      }
+
+      #drop--image {
+        top: -4rem;
+
+        #drop--toyo--title {
+          top: 32rem;
+        }
+
+        #drop--counter {
+          top: 56rem;
+        }
+      }
+    }
+
+    @media screen and (min-height: 801px) and (max-height: 860px) {
+      min-height: 180vh;
+      max-height: 190vh;
+
+      #drop--bg {
+        img {
+          height: 86.5%;
+        }
+      }
+
+      #drop--paragraph {
+        top: 88rem;
+      }
+
+      #drop--image {
+        top: -5rem;
+
+        #drop--toyo--title {
+          top: 33.5rem;
+          height: 8rem;
+        }
+
+        #drop--counter {
+          top: 56rem;
+        }
+      }
+
+      #drop--samurai--bg {
+        img {
+          top: 10rem;
+        }
+      }
+    }
+
+    @media screen and (min-width: 376px) and (min-height: 840px) {
+      #drop--bg {
+        img {
+          height: 84%;
+        }
+      }
+
+      #drop--paragraph {
+        top: 93rem;
+      }
+
+      #drop--image {
+        #drop--samurai {
+          top: 0.5rem;
+        }
+
+        #drop--toyo--title {
+          top: 35rem;
+        }
+
+        #drop--counter {
+          top: 61rem;
+        }
+      }
+
+      #drop--samurai--bg {
+        img {
+          top: 15rem;
+        }
+      }
+    }
+
+    @media screen and (min-width: 412px) and (max-height: 940px) {
+      min-height: 190vh;
+      max-height: 210vh;
+
+      #drop--bg {
+        img {
+          height: 80.5%;
+        }
+      }
+
+      #drop--paragraph {
+        top: 111rem;
+      }
+
+      #drop--image {
+        top: 4rem;
+
+        #drop--toyo--title {
+          top: 38.5rem;
+          height: 8rem;
+        }
+
+        #drop--counter {
+          top: 64rem;
+        }
+      }
+
+      #drop--samurai--bg {
+        img {
+          top: 25rem;
+        }
+      }
+    }
+
+    @media screen and (min-width: 768px) and (max-height: 1080px) {
+      height: 250vh;
+      max-height: 260vh;
 
       #drop--bg {
         img {
@@ -480,33 +871,79 @@ const Body = styled.div`
         width: 100%;
         padding: 0 2.5rem;
         left: 0;
-        top: 114rem;
+        top: 134rem;
         font-size: 1.84rem;
       }
 
       #drop--image {
         #drop--samurai {
-          left: -5rem;
-          width: 125%;
+          left: -17rem;
+          width: 175%;
         }
 
         #drop--toyo--title {
+          top: 51rem;
           height: 12rem;
         }
-      }
 
-      #drop--counter {
-        top: 66rem;
-        left: 2rem;
-        font-size: 8rem;
-        color: #fff;
+        #drop--counter {
+          top: 80rem;
+          left: 2rem;
+          font-size: 8rem;
+          color: #fff;
+        }
       }
 
       #drop--samurai--bg {
         img {
-          top: 38rem;
+          top: 44rem;
           left: 12rem;
           width: 175%;
+        }
+      }
+    }
+
+    @media screen and (min-width: 1080px) and (max-height: 810px) {
+      min-height: 190vh;
+      max-height: 200vh;
+
+      #drop--bg {
+        img {
+          height: 88%;
+        }
+      }
+
+      #drop--paragraph {
+        top: 118rem;
+      }
+
+      #drop--image {
+        top: -6rem;
+        left: 6rem;
+        #drop--samurai {
+          img {
+            width: 80%;
+          }
+        }
+
+        #drop--toyo--title {
+          top: 53.5rem;
+          left: -5.5rem;
+          height: 16rem;
+        }
+
+        #drop--counter {
+          top: 86rem;
+          left: -2rem;
+          font-size: 8.76rem;
+        }
+      }
+
+      #drop--samurai--bg {
+        img {
+          top: 54rem;
+          left: 32rem;
+          width: 100%;
         }
       }
     }
@@ -817,7 +1254,101 @@ const Body = styled.div`
       }
     }
 
-    @media screen and (min-width: 768px) and (min-height: 1024px) {
+    @media screen and (min-height: 667px) and (max-height: 760px) {
+      height: 245vh;
+      max-height: 255vh;
+    }
+
+    @media screen and (min-height: 761px) and (max-height: 800px) {
+      height: 225vh;
+      max-height: 240vh;
+    }
+
+    @media screen and (min-height: 801px) and (max-height: 860px) {
+      height: 215vh;
+      max-height: 225vh;
+
+      #wat--bg {
+        img {
+          top: -3rem;
+        }
+      }
+    }
+
+    @media screen and (max-width: 390px) and (min-height: 840px) {
+      height: 225vh;
+      max-height: 235vh;
+
+      #wat--player {
+        img {
+          top: -13rem;
+        }
+      }
+
+      #wat--wrapper {
+        #wat--desc {
+          top: -12rem;
+        }
+
+        #wat--paragraph {
+          top: -9rem;
+          width: 40%;
+        }
+
+        #wat--discord--btn {
+          top: 0;
+        }
+      }
+
+      #wat--toyo {
+        top: 38rem;
+      }
+
+      #wat--bg {
+        img {
+          top: 3rem;
+        }
+      }
+    }
+
+    @media screen and (min-width: 412px) and (max-height: 940px) {
+      height: 235vh;
+      max-height: 245vh;
+
+      #wat--toyo {
+        top: 41rem;
+        left: -3rem;
+
+        img {
+          width: 120%;
+        }
+      }
+
+      #wat--player {
+        img {
+          top: -13.5rem;
+          left: 7.5rem;
+        }
+      }
+
+      #wat--wrapper {
+        #wat--paragraph {
+          top: -10rem;
+          left: 6rem;
+        }
+
+        #wat--discord--btn {
+          top: 6rem;
+        }
+      }
+
+      #wat--bg {
+        top: 86rem;
+        left: 9rem;
+      }
+    }
+
+    @media screen and (min-width: 768px) and (max-height: 1080px) {
       height: 290vh;
       max-height: 310vh;
 
@@ -833,7 +1364,7 @@ const Body = styled.div`
       }
 
       #wat--toyo {
-        top: 64rem;
+        top: 66rem;
         left: 3rem;
 
         img {
@@ -867,11 +1398,63 @@ const Body = styled.div`
       }
 
       #wat--bg {
-        top: 110rem;
-        left: -7rem;
+        top: 116rem;
+        left: -5rem;
 
         img {
           width: 600%;
+        }
+      }
+    }
+
+    @media screen and (min-width: 1080px) and (max-height: 810px) {
+      top: 122vh;
+      height: 370vh;
+      max-height: 380vh;
+
+      #wat--player {
+        img {
+          top: -35rem;
+          left: 20rem;
+        }
+      }
+
+      #wat--title {
+        top: 2rem;
+      }
+
+      #wat--toyo {
+        top: 78rem;
+        left: 6rem;
+        width: 12rem;
+      }
+
+      #wat--wrapper {
+        #wat--desc {
+          width: 80%;
+          top: -34rem;
+        }
+
+        #wat--paragraph {
+          top: -30rem;
+          left: 13rem;
+        }
+
+        #wat--discord--btn {
+          top: -30rem;
+          left: 6rem;
+          width: 40%;
+          height: 8rem;
+        }
+      }
+
+      #wat--bg {
+        top: 40rem;
+
+        img {
+          top: 50rem;
+          left: 30rem;
+          width: 375%;
         }
       }
     }
